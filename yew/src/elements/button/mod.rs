2115ab24aa2bbea5dc4f7919b1ef1db74777d001
Matt Wisniewski <infamousvaguerat@gmail.com>
2024-02-08 23:13:40 -0500

    chore(Yew): Started working on basic button template

--- yew/src/elements/button/mod.rs
+++ yew/src/elements/button/mod.rs
@@ -10,11 +10,15 @@ pub struct Props {
 }
 
 #[function_component(Button)]
-pub fn butotn(props: &Props) -> Html {
+pub fn button(props: &Props) -> Html {
     let Props { text, on_press , appearance} = props.clone();
 
     html! {
-        <button class={format!("button {}", appearance.to_string())} onclick={move |e: MouseEvent|  on_press.emit(e)}>
+        <link data-trunk rel="sass" href="style.scss" />
+        <button
+            class={format!("button {}", appearance.to_string())}
+            onclick={move |e: MouseEvent|  on_press.emit(e)}
+        >
             {text}
         </button>
     }
