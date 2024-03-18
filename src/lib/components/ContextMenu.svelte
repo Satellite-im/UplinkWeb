f4a882768d454452158b8b5402451ca0f8a6aa50
jekrimo <jekrimo@gmail.com>
2024-03-18 14:36:45 -0600

    enable click on conext menu

--- src/lib/components/ContextMenu.svelte
+++ src/lib/components/ContextMenu.svelte
@@ -5,9 +5,55 @@
     import type { ContextItem } from "$lib/types"
 
     export let items: ContextItem[] = []
-</script>
 
-<div class="context-menu">
+    // pos is cursor position when right click occur
+    let pos = { x: 0, y: 0 }
+    // menu is dimension (height and width) of context menu
+    let menu = { h: 0, y: 0 }
+    // browser/window dimension (height and width)
+    let browser = { h: 0, y: 0 }
+    // showMenu is state of context-menu visibility
+    let showMenu = false;
+
+    function rightClickContextMenu(e: { clientX: any; clientY: any; }){
+        showMenu = true
+        browser = {
+            w: window.innerWidth,
+            h: window.innerHeight
+        };
+        pos = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        // If bottom part of context menu will be displayed
+        // after right-click, then change the position of the
+        // context menu. This position is controlled by `top` and `left`
+        // at inline style. 
+        // Instead of context menu is displayed from top left of cursor position
+        // when right-click occur, it will be displayed from bottom left.
+        if (browser.h -  pos.y < menu.h)
+            pos.y = pos.y - menu.h
+        if (browser.w -  pos.x < menu.w)
+            pos.x = pos.x - menu.w
+    }
+    function onPageClick(e: any){
+        // To make context menu disappear when
+        // mouse is clicked outside context menu
+        showMenu = false;
+    }
+    function getContextMenuDimension(node: HTMLDivElement){
+        // This function will get context menu dimension
+        // when navigation is shown => showMenu = true
+        let height = node.offsetHeight
+        let width = node.offsetWidth
+        menu = {
+            h: height,
+            w: width
+        }
+    }
+</script>
+{#if showMenu}
+<div class="context-menu" use:getContextMenuDimension style="position: absolute; top:{pos.y}px; left:{pos.x}px">
     <div class="header">
         <slot></slot>
     </div>
@@ -17,6 +63,7 @@
         </Button>
     {/each}
 </div>
+{/if}
 
 <style lang="scss">
     .context-menu {
@@ -43,3 +90,5 @@
         }
     }
 </style>
+<svelte:window on:contextmenu|preventDefault={rightClickContextMenu} 
+on:click={onPageClick} />
