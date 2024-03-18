c154a09f39579226bcfff8cbb688774ee04dba1b
Matt Wisniewski <infamousvaguerat@gmail.com>
2024-03-18 16:32:49 -0400

    reorg components

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,32 +1,25 @@
-export {default as Banner} from "$lib/components/Banner.svelte"
+export {default as Banner} from "$lib/components/ui/Banner.svelte"
 export {default as CallControls} from "$lib/components/CallControls.svelte"
-export {default as ChatPreview} from "$lib/components/ChatPreview.svelte"
+export {default as ChatPreview} from "$lib/components/messaging/ChatPreview.svelte"
 export {default as CoinBalance} from "$lib/components/CoinBalance.svelte"
-export {default as ColorSwatch} from "$lib/components/ColorSwatch.svelte"
-export {default as Container} from "$lib/components/Container.svelte"
-export {default as Conversation} from "$lib/components/Conversation.svelte"
-export {default as Key} from "$lib/components/Key.svelte"
-export {default as Message} from "$lib/components/Message.svelte"
-export {default as MessageContainer} from "$lib/components/MessageContainer.svelte"
-export {default as MessageGroup} from "$lib/components/MessageGroup.svelte"
-export {default as MessageReactions} from "$lib/components/MessageReactions.svelte"
-export {default as MessageReplyContainer} from "$lib/components/MessageReplyContainer.svelte"
-export {default as Meter} from "$lib/components/Meter.svelte"
-export {default as NewPayment} from "$lib/components/NewPayment.svelte"
-export {default as OrderedPhrase} from "$lib/components/OrderedPhrase.svelte"
-export {default as PaymentSuccessSplash} from "$lib/components/PaymentSuccessSplash.svelte"
+export {default as ColorSwatch} from "$lib/components/ui/ColorSwatch.svelte"
+export {default as Container} from "$lib/components/ui/Container.svelte"
+export {default as Conversation} from "$lib/components/messaging/Conversation.svelte"
+export {default as Key} from "$lib/components/settings/Key.svelte"
+export {default as Meter} from "$lib/components/settings/Meter.svelte"
+export {default as NewPayment} from "$lib/components/payments/NewPayment.svelte"
+export {default as OrderedPhrase} from "$lib/components/settings/OrderedPhrase.svelte"
+export {default as PaymentSuccessSplash} from "$lib/components/payments/PaymentSuccessSplash.svelte"
 export {default as PinInput} from "$lib/components/PinInput.svelte"
-export {default as ProfilePicture} from "$lib/components/ProfilePicture.svelte"
-export {default as ProgressBar} from "$lib/components/ProgressBar.svelte"
-export {default as ProgressButton} from "$lib/components/ProgressButton.svelte"
-export {default as Titlebar} from "$lib/components/Titlebar.svelte"
-export {default as ContextMenu} from "$lib/components/ContextMenu.svelte"
-
-
-
-
-
-
-
+export {default as ProfilePicture} from "$lib/components/profile/ProfilePicture.svelte"
+export {default as ProgressBar} from "$lib/components/ui/ProgressBar.svelte"
+export {default as ProgressButton} from "$lib/components/ui/ProgressButton.svelte"
+export {default as Titlebar} from "$lib/components/ui/Titlebar.svelte"
+export {default as ContextMenu} from "$lib/components/ui/ContextMenu.svelte"
+export {default as Message} from "$lib/components/messaging/message/Message.svelte"
+export {default as MessageReactions} from "$lib/components/messaging/message/MessageReactions.svelte"
+export {default as MessageContainer} from "$lib/components/messaging/MessageContainer.svelte"
+export {default as MessageGroup} from "$lib/components/messaging/MessageGroup.svelte"
+export {default as MessageReplyContainer} from "$lib/components/messaging/MessageReplyContainer.svelte"
 
 
