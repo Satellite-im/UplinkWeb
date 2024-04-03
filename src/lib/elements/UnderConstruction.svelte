c5433939d0dc6175b5bc073bcac8aac488148b0f
Matt Wisniewski <infamousvaguerat@gmail.com>
2024-04-03 17:25:23 -0400

    unify svg paths

---
+++ src/lib/elements/UnderConstruction.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Appearance, Shape } from "$lib/enums"
+    import Icon from "./Icon.svelte"
+    import Text from "./Text.svelte";
+
+</script>
+
+<div class="under-construction">
+    <Icon icon={Shape.Construction} highlight={Appearance.Warning} />
+    <Text appearance={Appearance.Warning}>Under Construction</Text>
+</div>
+
+<style lang="scss">
+    .under-construction {
+        display: inline-flex;
+        gap: var(--gap);
+        justify-content: center;
+        align-items: center;
+    }
+</style>
