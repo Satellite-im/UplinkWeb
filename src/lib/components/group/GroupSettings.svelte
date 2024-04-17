f4480e74c436e1f984bf2a64b6c1a71341d5a6de
Matt Wisniewski <infamousvaguerat@gmail.com>
2024-04-17 14:24:09 -0400

    feat(Groups): Added group settings

---
+++ src/lib/components/group/GroupSettings.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Label from "$lib/elements/Label.svelte"
+    import SettingSection from "$lib/layouts/SettingSection.svelte"
+    import Switch from "$lib/elements/Switch.svelte"
+</script>
+
+<div class="settings">
+    <Label text="Settings" />
+    <SettingSection name="Add Members" description="Allow anyone to add members to the group">
+        <Switch on />
+    </SettingSection>
+    <SettingSection name="Change Details" description="Allow anyone to change the name and motd of the group">
+        <Switch />
+    </SettingSection>
+    <SettingSection name="Change Photo" description="Allow anyone to change the group photo">
+        <Switch />
+    </SettingSection>
+</div>
+
+<style lang="scss">
+    .settings {
+        display: inline-flex;
+        flex-direction: column;
+        padding: var(--padding);
+        gap: var(--gap);
+
+    }
+</style>
