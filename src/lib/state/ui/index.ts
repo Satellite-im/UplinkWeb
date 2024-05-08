95c5a5a04a04edb02925ca637fef2077677e52de
lgmarchi <lucasgmarchi@gmail.com>
2024-05-08 17:39:00 -0300

    Added warp-wasm as module

--- src/lib/state/ui/index.ts
+++ src/lib/state/ui/index.ts
@@ -2,8 +2,8 @@ import type { Chat } from "$lib/types"
 import { get, type Writable } from "svelte/store"
 import { createPersistentState } from ".."
 import { Font } from "$lib/enums"
-import { identity_set_created }  from "$lib/pkg/warp_bg.wasm";
-import init, * as wasm from '$lib/pkg/warp.js';
+import { identity_set_created }  from "../../../../warp-wasm/pkg/warp_bg.wasm";
+
 
 export interface IUIState {
     color: Writable<string>,
