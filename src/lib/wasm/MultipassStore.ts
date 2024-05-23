8ef4138de9f8df4877449a5b6a64d05d123e549b
lgmarchi <lucasgmarchi@gmail.com>
2024-05-23 15:33:50 -0300

    Remove ;

--- src/lib/wasm/MultipassStore.ts
+++ src/lib/wasm/MultipassStore.ts
@@ -4,80 +4,80 @@ import { WarpStore } from "./WarpStore"
 
 
 class MultipassStore {
-    private multipassWritable: Writable<wasm.MultiPassBox | null>;
-    private identity: Writable<wasm.Identity | null> = writable(null);
+    private multipassWritable: Writable<wasm.MultiPassBox | null>
+    private identity: Writable<wasm.Identity | null> = writable(null)
 
     constructor(multipass: Writable<wasm.MultiPassBox | null>) {
-        this.multipassWritable = multipass;
+        this.multipassWritable = multipass
     }
 
     async createIdentity(username: string, statusMessage: string, passphrase: string | undefined): Promise<void> {
-        console.log('Started creating identity');
-        const multipass = get(this.multipassWritable);
+        console.log('Started creating identity')
+        const multipass = get(this.multipassWritable)
 
         if (multipass) {
             try {
-                await multipass.create_identity(username, passphrase);
-                await this.updateStatusMessage(statusMessage);
-                const identity = get(this.identity);
-                console.log('New account created. Username: ', identity?.username());
+                await multipass.create_identity(username, passphrase)
+                await this.updateStatusMessage(statusMessage)
+                const identity = get(this.identity)
+                console.log('New account created. Username: ', identity?.username())
             } catch (error) {
-                console.error('Error creating identity: ', error);
+                console.error('Error creating identity: ', error)
             }
         }
     }
 
     async getOwnIdentity(): Promise<wasm.Identity | undefined> {
-        const multipass = get(this.multipassWritable);
+        const multipass = get(this.multipassWritable)
 
         if (multipass) {
             try {
-                const identity = await multipass.get_own_identity();
-                return identity;
+                const identity = await multipass.get_own_identity()
+                return identity
             } catch (error) {
-                console.error('Error getting own identity: ', error);
-                return undefined;
+                console.error('Error getting own identity: ', error)
+                return undefined
             }
         }
         return undefined;
     }
 
     async updateUsername(new_username: string) {
-        const multipass = get(this.multipassWritable);
+        const multipass = get(this.multipassWritable)
 
         if (multipass) {
-            await multipass.update_identity(wasm.IdentityUpdate.Username, new_username);
-            await this._updateIdentity();
+            await multipass.update_identity(wasm.IdentityUpdate.Username, new_username)
+            await this._updateIdentity()
         }
     }
 
     async updateStatusMessage(new_status_message: string) {
-        const multipass = get(this.multipassWritable);
+        const multipass = get(this.multipassWritable)
 
         if (multipass) {
-            await multipass.update_identity(wasm.IdentityUpdate.StatusMessage, new_status_message);
-            await this._updateIdentity();
+            await multipass.update_identity(wasm.IdentityUpdate.StatusMessage, new_status_message)
+            await this._updateIdentity()
         }
     }
 
     async updateProfilePhoto(new_photo: string) {
-        const multipass = get(this.multipassWritable);
+        const multipass = get(this.multipassWritable)
 
         if (multipass) {
-            await multipass.update_identity(wasm.IdentityUpdate.Picture, new_photo);
-            await this._updateIdentity();
+            await multipass.update_identity(wasm.IdentityUpdate.Picture, new_photo)
+            await this._updateIdentity()
         }
     }
 
     private async _updateIdentity() {
-        const multipass = get(this.multipassWritable);
+        const multipass = get(this.multipassWritable)
 
         if (multipass) {
             try {
-                const updated_identity = await multipass.get_own_identity();
-                this.identity.update(() => updated_identity);
+                const updated_identity = await multipass.get_own_identity()
+                this.identity.update(() => updated_identity)
             } catch (error) {
-                console.error('Error updating identity: ', error);
+                console.error('Error updating identity: ', error)
             }
         }
     }
--- src/lib/wasm/TesseractStore.ts
+++ src/lib/wasm/TesseractStore.ts
@@ -2,41 +2,41 @@ import { get, writable, type Writable } from "svelte/store"
 import init, * as wasm from "warp-wasm"
 
 class TesseractStore {
-    private tesseractWritable: Writable<wasm.Tesseract | null> = writable(null);
+    private tesseractWritable: Writable<wasm.Tesseract | null> = writable(null)
 
     async getTesseract(): Promise<wasm.Tesseract> {
-        return get(this.tesseractWritable)!;
+        return get(this.tesseractWritable)!
     }
 
     async unlock(pin: string) {
-        await init();
-        console.log('Pin to unlock Tesseract: ', pin);
-        const tesseractInstance = new wasm.Tesseract();
-        this.tesseractWritable.set(tesseractInstance);
+        await init()
+        console.log('Pin to unlock Tesseract: ', pin)
+        const tesseractInstance = new wasm.Tesseract()
+        this.tesseractWritable.set(tesseractInstance)
         
-        const encoder = new TextEncoder();
-        const passphrase = encoder.encode(pin);
+        const encoder = new TextEncoder()
+        const passphrase = encoder.encode(pin)
         
         try {
-            await tesseractInstance.load_from_storage();
-            await tesseractInstance.unlock(passphrase);
+            await tesseractInstance.load_from_storage()
+            await tesseractInstance.unlock(passphrase)
 
             if (!tesseractInstance.autosave_enabled()) {
-                tesseractInstance.set_autosave();
+                tesseractInstance.set_autosave()
             }
             
-            console.log('Tesseract: ', tesseractInstance);
+            console.log('Tesseract: ', tesseractInstance)
         } catch (error) {
-            console.error('Error unlocking Tesseract: ', error);
+            console.error('Error unlocking Tesseract: ', error)
         }
     }
 
     lock() {
-        const tesseractInstance = get(this.tesseractWritable);
+        const tesseractInstance = get(this.tesseractWritable)
         if (tesseractInstance) {
-            tesseractInstance.lock();
+            tesseractInstance.lock()
         }
     }
 }
 
-export const TesseractStoreInstance = new TesseractStore();
+export const TesseractStoreInstance = new TesseractStore()
