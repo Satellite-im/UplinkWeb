be17d455f9857e586d7083e39ba25a5dc4bcca76
lgmarchi <lucasgmarchi@gmail.com>
2024-05-29 21:38:43 -0300

    Handle with Friend request already exist

--- src/lib/wasm/handle_errors.ts
+++ src/lib/wasm/handle_errors.ts
@@ -1,3 +1,4 @@
+import { ULog } from "../../ulog"
 
 export enum WarpError {
     SELF_REQUEST = 'You cannot send yourself a friend request',
@@ -7,11 +8,12 @@ export enum WarpError {
 }
 
 export function handleErrors(error: any): WarpError{
+    ULog.error('Error: ', error)
     if (error instanceof Error) {
         if (error.message.includes('You cannot send yourself a friend request')) {
             return WarpError.SELF_REQUEST
         }
-        if (error.message.includes('Send request already sent')) {
+        if (error.message.includes('Friend request already exist')) {
             return WarpError.SEND_REQUEST_ALREADY_SENT
         }
     }
