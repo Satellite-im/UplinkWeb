fe9855499ecbe99ccd2a3e061743a62e8f696eb1
lgmarchi <lucasgmarchi@gmail.com>
2024-05-31 17:34:48 -0300

    Add 2 more errors from warp

--- src/lib/wasm/HandleWarpErrors.ts
+++ src/lib/wasm/HandleWarpErrors.ts
@@ -4,6 +4,8 @@ export enum WarpError {
     SELF_REQUEST = 'You cannot send yourself a friend request',
     FRIEND_REQUEST_ALREADY_EXIST = 'Friend request already exist',
     IDENTITY_NOT_CREATED = 'Identity has not been created',
+    USER_HAS_BLOCKED_YOU = 'User has blocked you from being able to interact with them',
+    USER_ALREADY_EXIST_AS_FRIEND = 'User already exist as a friend',
     GENERAL_ERROR = 'An unknown error occurred',
     MULTIPASS_NOT_FOUND = 'Multipass instance not found',
 }
@@ -17,16 +19,20 @@ const errorMessages = {
 export function handleErrors(error: any): WarpError{
     ULog.error('Error: ', error)
     if (error instanceof Error) {
-        const message = error.message;
+        const message = error.message
         switch (true) {
             case message.includes(WarpError.SELF_REQUEST):
-                return WarpError.SELF_REQUEST;
+                return WarpError.SELF_REQUEST
             case message.includes(WarpError.FRIEND_REQUEST_ALREADY_EXIST):
-                return WarpError.FRIEND_REQUEST_ALREADY_EXIST;
+                return WarpError.FRIEND_REQUEST_ALREADY_EXIST
             case message.includes(WarpError.IDENTITY_NOT_CREATED):
-                return WarpError.IDENTITY_NOT_CREATED;
+                return WarpError.IDENTITY_NOT_CREATED
+            case message.includes(WarpError.USER_HAS_BLOCKED_YOU):
+                return WarpError.USER_HAS_BLOCKED_YOU
+            case message.includes(WarpError.USER_ALREADY_EXIST_AS_FRIEND):
+                return WarpError.USER_ALREADY_EXIST_AS_FRIEND
             default:
-                return WarpError.GENERAL_ERROR;
+                return WarpError.GENERAL_ERROR
         }
     }
     return WarpError.GENERAL_ERROR
