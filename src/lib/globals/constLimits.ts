df5d00b111f27fd4de7fceefba7db38bddd39dcf
ashneverdawn <8341280+ashneverdawn@users.noreply.github.com>
2024-06-17 14:18:57 -0400

    add warp constants

---
+++ src/lib/globals/constLimits.ts
@@ -0,0 +1,12 @@
+export const MAX_THUMBNAIL_SIZE: number = 5_242_880;
+export const MAX_IMAGE_SIZE: number = 2_097_152;
+export const MIN_USERNAME_LENGTH: number = 4;
+export const MAX_USERNAME_LENGTH: number = 64;
+export const MAX_STATUS_LENGTH: number = 512;
+export const MIN_MESSAGE_SIZE: number = 1;
+export const MAX_MESSAGE_SIZE: number = 4_096;
+export const MAX_ATTACHMENT: number = 32;
+export const MIN_ATTACHMENT: number = 1;
+export const MAX_CONVERSATIONS: number = 1_000;
+export const MAX_FRIENDS: number = 1_000;
+export const MAX_REQUEST: number = 1_000;
