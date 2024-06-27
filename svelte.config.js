a465cb0b1102e23fc8064ffdec544743bd1d756b
lgmarchi <lucasgmarchi@gmail.com>
2024-06-27 12:34:06 -0300

    test 9

--- svelte.config.js
+++ svelte.config.js
@@ -13,13 +13,13 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $lib: path.resolve('src/lib')
-        }
-      }
-    }
+    // vite: {
+    //   resolve: {
+    //     alias: {
+    //       $lib: path.resolve('src/lib')
+    //     }
+    //   }
+    // }
   },
 }
 
