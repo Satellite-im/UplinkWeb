70486750f58c3a85440c03e9b94912789f09835c
lgmarchi <lucasgmarchi@gmail.com>
2024-06-27 12:33:33 -0300

    test 9

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 import adapter from '@sveltejs/adapter-static'
+import path from "node:path"
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
