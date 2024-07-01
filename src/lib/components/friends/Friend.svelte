7c25ded807ef6584362443f616caf52be9b2b01f
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-07-01 12:09:47 -0600

    add(attributes): test attributes for friends pages

--- src/lib/components/friends/Friend.svelte
+++ src/lib/components/friends/Friend.svelte
@@ -12,9 +12,9 @@
     export let friend: User = defaultUser
 </script>
 
-<div class="friend">
-    <ProfilePicture size={Size.Small} image={friend.profile.photo.image} status={friend.profile.status} />
-    <Text class="username" singleLine>
+<div class="friend" data-cy="friend-{friend.name}">
+    <ProfilePicture hook="friend-profile-picture" size={Size.Small} image={friend.profile.photo.image} status={friend.profile.status} />
+    <Text hook="friend-name" class="username" singleLine>
         {friend.name}
     </Text>
     <Controls>
