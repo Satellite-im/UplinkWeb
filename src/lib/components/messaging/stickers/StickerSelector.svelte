c4233ffeffd1967b8cb525670aa4ca008d1cda81
Matt Wisniewski <infamousvaguerat@gmail.com>
2024-07-02 03:24:39 -0400

    stuff

--- src/lib/components/messaging/stickers/StickerSelector.svelte
+++ src/lib/components/messaging/stickers/StickerSelector.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte"
     import Label from "$lib/elements/Label.svelte"
-
     import defaultManifest from "./manifest.json"
     import { Icon, Input } from "$lib/elements"
     import { Shape } from "$lib/enums"
@@ -26,109 +25,140 @@
 </script>
 
 <div class="sticker-selector">
-    <div class="header">
-        <!-- svelte-ignore missing-declaration -->
-        <Input alt placeholder={$_("generic.search_placeholder")} bind:value={$searchQuery}>
-            <Icon icon={Shape.Search} />
-        </Input>
-    </div>
-    <div class="stickers">
+    <div class="sidebar">
+        <Label text="Packs" />
+
         {#each stickers as collection}
-            <Label text={`${collection.name} (${collection.author})`} />
-            <div class="collection-items">
-                {#each collection.assets as sticker}
-                    <button class="sticker-item" on:click={() => handleStickerClick(sticker)} on:keydown={event => handleKeyDown(event, sticker)} tabindex="0" aria-label={`Select sticker ${sticker.name}`}>
-                        <img src={sticker.path} alt={sticker.name} />
-                        <Label class="label" text={sticker.name} />
-                    </button>
-                {/each}
-            </div>
-            <Spacer />
+            <a href={`#${collection.name}`} class="collection-link" aria-label={`Jump to ${collection.name}`}>
+                <img src={collection.assets[0].path} alt={collection.name} />
+            </a>
         {/each}
+    </div>
+    <div class="content">
+        <div class="header">
+            <!-- svelte-ignore missing-declaration -->
+            <Input alt placeholder={$_("generic.search_placeholder")} bind:value={$searchQuery}>
+                <Icon icon={Shape.Search} />
+            </Input>
+        </div>
+        <div class="stickers">
+            {#each stickers as collection}
+                <section id={collection.name}>
+                    <Label text={`${collection.name} (${collection.author})`} />
+                    <div class="collection-items">
+                        {#each collection.assets as sticker}
+                            <button class="sticker-item" on:click={() => handleStickerClick(sticker)} on:keydown={event => handleKeyDown(event, sticker)} tabindex="0" aria-label={`Select sticker ${sticker.name}`}>
+                                <img src={sticker.path} alt={sticker.name} />
+                                <Label class="label" text={sticker.name} />
+                            </button>
+                        {/each}
+                    </div>
+                    <Spacer />
+                </section>
+            {/each}
+        </div>
     </div>
 </div>
 
 <style lang="scss">
     .sticker-selector {
-        display: inline-flex;
-        flex-direction: column;
+        display: flex;
         height: var(--emoji-selector-height);
-        width: calc(var(--min-component-width) * 2);
-        gap: var(--gap);
+        width: 100%;
+        max-width: var(--max-component-width);
+    }
 
-        .stickers {
+    .sidebar {
+        display: flex;
+        flex-direction: column;
+        gap: var(--gap-less);
+        overflow-y: auto;
+        background: var(--background-color);
+        border-right: var(--border-width) solid var(--border-color);
+        align-items: center;
+        padding-right: var(--gap);
+
+        .collection-link {
             display: flex;
-            flex-wrap: wrap;
-            gap: var(--gap-less);
             align-items: center;
-            flex: 1;
-            overflow-y: auto;
-            overflow-x: hidden;
+            justify-content: center;
+            margin-bottom: var(--gap-less);
 
-            .collection-items {
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: space-between;
-                width: calc(var(--min-component-width) * 2);
+            img {
+                width: 40px;
+                height: 40px;
+                border-radius: 5px;
+                transition:
+                    transform 0.3s,
+                    filter 0.3s;
             }
-        }
 
-        .search-bar {
-            position: sticky;
-            top: 0;
-            background: var(--background-color);
-            z-index: 1;
-            width: 100%;
-
-            .header {
-                display: inline-flex;
-                flex-direction: row;
-                width: 100%;
-                gap: var(--gap);
+            &:hover img,
+            &:focus img {
+                transform: scale(1.1);
+                filter: brightness(1.2);
             }
         }
     }
 
-    .sticker-item {
+    .content {
         display: flex;
         flex-direction: column;
-        align-items: center;
-        cursor: pointer;
-        transition: transform 0.3s;
-        background: none;
-        border: none;
-        padding: var(--padding-less);
+        flex: 1;
 
-        &:hover,
-        &:focus {
-            transform: scale(1.1);
-            outline: none;
-            filter: brightness(1.2);
-        }
-        &:focus::after {
-            content: "";
-            position: absolute;
-            top: var(--gap);
-            left: var(--gap);
-            transform: translateX(-50%);
-            width: 0.5rem;
-            height: 0.5rem;
-            background-color: var(--primary-color-alt);
-            border-radius: 50%;
-            box-shadow: 0 0 8px var(--primary-color-alt);
+        .header {
+            position: sticky;
+            top: 0;
+            background: var(--background-color);
+            z-index: 1;
+            padding: 0 var(--padding-less);
+
+            input {
+                width: 100%;
+            }
         }
 
-        img {
-            width: var(--sticker-width);
-            height: auto;
-            border-radius: 5px;
-        }
+        .stickers {
+            padding: var(--padding);
+            display: flex;
+            flex-direction: column;
+            gap: var(--gap-less);
+            overflow-y: auto;
 
-        .label {
-            margin-top: 5px;
-            text-align: center;
-            color: var(--primary-color);
-            font-size: 14px;
+            section {
+                margin-bottom: 1rem;
+
+                .collection-items {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: space-between;
+                    gap: var(--gap-less);
+                }
+            }
+
+            .sticker-item {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                cursor: pointer;
+                transition: transform 0.3s;
+                background: none;
+                border: none;
+                padding: var(--padding-less);
+
+                &:hover,
+                &:focus {
+                    transform: scale(1.1);
+                    outline: none;
+                    filter: brightness(1.2);
+                }
+
+                img {
+                    width: var(--sticker-width);
+                    height: auto;
+                    border-radius: 5px;
+                }
+            }
         }
     }
 </style>
