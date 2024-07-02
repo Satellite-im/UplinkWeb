463c1d3769a985d01981ca5097fa266d7b947e01
Matt Wisniewski <infamousvaguerat@gmail.com>
2024-07-01 20:26:24 -0400

    fix semicolons

--- src/lib/utils/CommonInputRules.ts
+++ src/lib/utils/CommonInputRules.ts
@@ -1,5 +1,5 @@
-import { InputRules } from "$lib/elements/Input/inputRules";
+import { InputRules } from "$lib/elements/Input/inputRules"
 
 export class CommonInputRules {
-    static username = new InputRules(4, 20, true, /^[a-zA-Z0-9]+$/)
-  }
+  static username = new InputRules(4, 20, true, /^[a-zA-Z0-9]+$/)
+}
