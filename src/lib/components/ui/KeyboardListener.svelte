aeee071ad2920521c3ac1a287119b4dac30f97d4
lgmarchi <lucasgmarchi@gmail.com>
2024-07-05 16:08:37 -0300

    Remove onKeyUp for keybinds

--- src/lib/components/ui/KeyboardListener.svelte
+++ src/lib/components/ui/KeyboardListener.svelte
@@ -26,29 +26,11 @@
         })
     }
 
-    function handleKeyUp(event: KeyboardEvent) {
-        let key = event.key
-        let modifiers: string[] = []
-
-        if (event.shiftKey) modifiers.push("shift")
-        if (event.ctrlKey) modifiers.push("ctrl")
-
-        dispatch("event", { key, modifiers, state: KeybindState.Released })
-
-        keybinds.forEach(keybind => {
-            if (keybind.key === key && keybind.modifiers.sort().join() === modifiers.sort().join()) {
-                dispatch("match", keybind)
-            }
-        })
-    }
-
     onMount(() => {
         window.addEventListener("keydown", handleKeyDown)
-        window.addEventListener("keyup", handleKeyUp)
     })
 
     onDestroy(() => {
         window.removeEventListener("keydown", handleKeyDown)
-        window.removeEventListener("keyup", handleKeyUp)
     })
 </script>
