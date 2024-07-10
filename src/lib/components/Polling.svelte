a1f87ff4a7a520c3abfdeb94e712cf7211a808e6
lgmarchi <lucasgmarchi@gmail.com>
2024-07-10 13:15:06 -0300

    Update user info on chats as well

--- src/lib/components/Polling.svelte
+++ src/lib/components/Polling.svelte
@@ -29,7 +29,7 @@
 
             for (let chat of chatsUI) {
                 let usersUpdated: User[] = []
-
+                
                 for (let user of chat.users) {
                     let friend = await MultipassStoreInstance.identity_from_did(user.key)
                     let userUpdated: User = {
@@ -67,6 +67,13 @@
 
                 if (activeChat.id === chat.id) {
                     Store.state.activeChat.set(chat)
+                    let conversation = ConversationStore.getConversation(activeChat)
+                    conversation?.messages.forEach(message => {
+                        message.messages.forEach(m => {
+                            m.details.origin = chat.users.find(u => u.key === m.details.origin.key) ?? m.details.origin
+                        })
+                    })
+                 
                 }
 
                 chatsUIUpdated.push(chat)
