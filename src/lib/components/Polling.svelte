6ab42e3ba70851264e444ecb905a54343a715fe0
lgmarchi <lucasgmarchi@gmail.com>
2024-07-10 15:43:58 -0300

    Improved code

--- src/lib/components/Polling.svelte
+++ src/lib/components/Polling.svelte
@@ -20,23 +20,23 @@
         poll()
     })
 
-
     async function updateUsersOnChats() {
         let chatsUI = get(UIStore.state.chats)
         let chatsUIUpdated: Chat[] = []
         let activeChat = get(Store.state.activeChat)
-        if (chatsUI.length !== 0) {
+        let updateChats = false
 
+        if (chatsUI.length !== 0) {
             for (let chat of chatsUI) {
                 let usersUpdated: User[] = []
-                
+
                 for (let user of chat.users) {
                     let friend = await MultipassStoreInstance.identity_from_did(user.key)
                     let userUpdated: User = {
-                            ...user,
-                            name: friend?.name ?? user.name,
-                            media: {
-                            ...user.media
+                        ...user,
+                        name: friend?.name ?? user.name,
+                        media: {
+                            ...user.media,
                         },
                         profile: {
                             ...user.profile,
@@ -44,42 +44,61 @@
                             status_message: friend?.profile.status_message ?? user.profile.status_message,
                             banner: {
                                 ...user.profile.banner,
-                                image: friend?.profile.banner.image ?? user.profile.banner.image
+                                image: friend?.profile.banner.image ?? user.profile.banner.image,
                             },
                             photo: {
                                 ...user.profile.photo,
-                                image: friend?.profile.photo.image ?? user.profile.photo.image
-                            }
-                        }
+                                image: friend?.profile.photo.image ?? user.profile.photo.image,
+                            },
+                        },
                     }
+
                     if (!friend?.profile.photo.image) {
-                        userUpdated.profile.photo.image = user.profile.photo.image;
+                        userUpdated.profile.photo.image = user.profile.photo.image
                     }
                     if (!friend?.profile.banner.image) {
-                        userUpdated.profile.banner.image = user.profile.banner.image;
+                        userUpdated.profile.banner.image = user.profile.banner.image
                     }
-                    usersUpdated.push(userUpdated)
-                }
-        
-                chat = {
-                    ...chat,
-                    users: usersUpdated
+
+                    if (!isUserEqual(user, userUpdated)) {
+                        updateChats = true
+                        usersUpdated.push(userUpdated)
+                    } else {
+                        usersUpdated.push(user)
+                    }
                 }
 
-                if (activeChat.id === chat.id) {
-                    Store.state.activeChat.set(chat)
-                    let conversation = ConversationStore.getConversation(activeChat)
-                    conversation?.messages.forEach(message => {
-                        message.messages.forEach(m => {
-                            m.details.origin = chat.users.find(u => u.key === m.details.origin.key) ?? m.details.origin
+                if (updateChats) {
+                    chat = {
+                        ...chat,
+                        users: usersUpdated,
+                    }
+
+                    if (activeChat.id === chat.id) {
+                        Store.state.activeChat.set(chat)
+                        let conversation = ConversationStore.getConversation(activeChat)
+                        conversation?.messages.forEach(message => {
+                            message.messages.forEach(m => {
+                                m.details.origin = chat.users.find(u => u.key === m.details.origin.key) ?? m.details.origin
+                            })
                         })
-                    })
-                 
+                    }
+                    chatsUIUpdated.push(chat)
                 }
-
-                chatsUIUpdated.push(chat)
             }
-            UIStore.state.chats.set(chatsUIUpdated)
+            if (updateChats) {
+                UIStore.state.chats.set(chatsUIUpdated)
+            }
         }
+    }
+
+    function isUserEqual(user1: User, user2: User): boolean {
+        return (
+            user1.name === user2.name &&
+            user1.profile.status === user2.profile.status &&
+            user1.profile.status_message === user2.profile.status_message &&
+            user1.profile.banner.image === user2.profile.banner.image &&
+            user1.profile.photo.image === user2.profile.photo.image
+        )
     }
 </script>
