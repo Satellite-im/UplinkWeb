e6d61966df0b190ef00e1fdf71e1a9c02eb108a7
Lucas Gonçalves Marchi <lucasgmarchi@gmail.com>
2024-07-17 15:36:31 -0300

    fix(Picture): Show profile picture when user erase its own picture (#255)
    Co-authored-by: Phill Wisniewski <93608357+phillsatellite@users.noreply.github.com>
    Co-authored-by: Sara Tavares <29093946+stavares843@users.noreply.github.com>
    Co-authored-by: Luis E <35935591+luisecm@users.noreply.github.com>

--- src/lib/wasm/MultipassStore.ts
+++ src/lib/wasm/MultipassStore.ts
@@ -574,7 +574,11 @@ class MultipassStore {
         if (multipass) {
             try {
                 let identityProfilePicture = await multipass.identity_picture(did)
-                profilePicture = identityProfilePicture ? this.to_base64(identityProfilePicture.data()) : ""
+                profilePicture = identityProfilePicture && identityProfilePicture.data 
+                ? (identityProfilePicture.data().length > 2 
+                    ? this.to_base64(identityProfilePicture.data()) 
+                    : "") 
+                : ""
             } catch (error) {
                 // log.error(`Couldn't fetch profile picture for ${did}: ${error}`)
             }
@@ -588,7 +592,11 @@ class MultipassStore {
         if (multipass) {
             try {
                 let identityBannerPicture = await multipass.identity_banner(did)
-                bannerPicture = identityBannerPicture ? this.to_base64(identityBannerPicture.data()) : ""
+                bannerPicture = identityBannerPicture && identityBannerPicture.data 
+                ? (identityBannerPicture.data().length > 2 
+                    ? this.to_base64(identityBannerPicture.data()) 
+                    : "") 
+                : ""
             } catch (error) {
                 // log.error(`Couldn't fetch banner picture for ${did}: ${error}`)
             }
