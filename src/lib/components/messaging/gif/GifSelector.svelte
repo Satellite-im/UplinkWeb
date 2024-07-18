3043bc0cdac7ff4c2f4cedb66e1c7e6e09d7e62d
jekrimo <jekrimo@gmail.com>
2024-07-18 09:17:49 -0600

    gify selector responsive

--- src/lib/components/messaging/gif/GifSelector.svelte
+++ src/lib/components/messaging/gif/GifSelector.svelte
@@ -243,7 +243,7 @@
         flex-direction: column;
         gap: 1rem;
         height: var(--emoji-selector-height);
-        width: calc(var(--min-component-width) * 2);
+        // width: calc(var(--min-component-width) * 2);
 
         .search-bar {
             position: sticky;
@@ -276,34 +276,41 @@
                 align-items: center;
                 justify-content: center;
                 position: relative;
+            }
 
-                &:hover .icon-container {
-                    visibility: visible;
-                }
+            .gif-container:hover .icon-container {
+                visibility: visible;
+            }
 
-                .icon-container {
-                    position: absolute;
-                    top: var(--gap-less);
-                    left: var(--gap-less);
-                    z-index: 3;
+            .icon-container {
+                position: absolute;
+                top: var(--gap-less);
+                left: var(--gap-less);
+                z-index: 3;
+            }
 
-                    &:hover {
-                        cursor: pointer;
-                    }
-                }
+            .icon-container:hover {
+                cursor: pointer;
+            }
 
+            .gif {
+                object-fit: fill;
+                cursor: pointer;
+                height: 133.33px;
+                width: 100%;
+                border: var(--border-width) solid var(--border-color);
+                border-radius: var(--border-radius-minimal);
+                transition: transform var(--animation-speed) ease;
+            }
+
+            .gif:hover {
+                transform: scale(1.05);
+            }
+
+            @media (max-width: 600px) {
                 .gif {
-                    object-fit: fill;
-                    cursor: pointer;
-                    height: 133.33px;
+                    height: 100px; /* Adjust height as needed */
                     width: 100%;
-                    border: var(--border-width) solid var(--border-color);
-                    border-radius: var(--border-radius-minimal);
-                    transition: transform var(--animation-speed) ease;
-
-                    &:hover {
-                        transform: scale(1.05);
-                    }
                 }
             }
 
