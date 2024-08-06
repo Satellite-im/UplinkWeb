d3881bdca26370e7850de8893518c9642ebe6e6e
lgmarchi <lucasgmarchi@gmail.com>
2024-08-06 13:45:55 -0300

    Fix integrationDisplay page

--- src/lib/components/ui/IntegrationDisplay.svelte
+++ src/lib/components/ui/IntegrationDisplay.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import { INTEGRATIONS } from "$lib/config"
+    import { toIntegrationIconSrc, toIntegrationKind } from "$lib/utils/ProfileUtils"
     import { Button, Icon, Input, Label } from "$lib/elements"
     import { Appearance, IntegrationDisplays, Integrations, Shape } from "$lib/enums"
-    import type { Integration } from "$lib/types"
 
-    export let integration: Integration
+    export let key: string
+    export let value: string
+    export let editable: boolean = false
 
-    let display = INTEGRATIONS[integration.kind].display
+    function display(key: string): IntegrationDisplays {
+        return INTEGRATIONS[toIntegrationKind(key)].display
+    }
 </script>
 
 <div class="integration" data-cy="account-integrations-item">
