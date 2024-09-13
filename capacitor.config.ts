6ddfcc7180720506f670d3885aabb71ad92feefe
lgmarchi <lucasgmarchi@gmail.com>
2024-09-12 23:13:47 -0300

    Build android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-  appId: 'com.uplink.app',
-  appName: 'Uplink',
-  webDir: 'dist'
-};
+    appId: "com.uplink.app",
+    appName: "Uplink",
+    webDir: "build",
+}
 
-export default config;
+export default config
