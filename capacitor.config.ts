a41e0cc5745dcea0eb74f67dfb7c2ca2118f8e4b
lgmarchi <lucasgmarchi@gmail.com>
2024-09-16 18:59:28 -0300

    Make app work on Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,12 @@ const config: CapacitorConfig = {
     appId: "com.uplink.app",
     appName: "Uplink",
     webDir: "build",
+    bundledWebRuntime: false,
+    server: {
+        // TODO(Mobile): Replace <your-machine-ip> with your machine's IP address
+        url: "http://<your-machine-ip>:5173",
+        cleartext: true,
+    },
 }
 
 export default config
