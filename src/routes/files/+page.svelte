105c201ac31a7fe9afb996a267b216b4b62b7284
Lucas Gonçalves Marchi <lucasgmarchi@gmail.com>
2024-09-25 16:06:34 -0300

    fix(Sibedar): Fix chat preview order on sidebar (#618)
    Co-authored-by: Sara Tavares <29093946+stavares843@users.noreply.github.com>
    Co-authored-by: Phill Wisniewski <93608357+phillsatellite@users.noreply.github.com>

--- src/routes/files/+page.svelte
+++ src/routes/files/+page.svelte
@@ -521,7 +521,11 @@
             </Button>
         </Controls>
         {#if activeTabRoute === "chats"}
-            {#each $chats as chat}
+            {#each $chats.slice().sort((a, b) => {
+                const dateA = new Date(a.last_message_at || 0)
+                const dateB = new Date(b.last_message_at || 0)
+                return dateB.getTime() - dateA.getTime()
+            }) as chat}
                 <ContextMenu
                     hook="context-menu-sidebar-chat"
                     items={[
