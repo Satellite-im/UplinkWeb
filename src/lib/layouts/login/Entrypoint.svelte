9f856aa443f50776c803b264c6c0be64cae916c5
lgmarchi <lucasgmarchi@gmail.com>
2024-10-03 12:30:52 -0300

    Hide change users button as well and just show it if user is on devmode

--- src/lib/layouts/login/Entrypoint.svelte
+++ src/lib/layouts/login/Entrypoint.svelte
@@ -73,9 +73,11 @@
 
     <div class="unlock-controls">
         <Controls>
-            <Button tooltip={$_("pages.auth.changeUser")} hook="button-change-user" icon on:click={_ => (showAccounts = true)} appearance={Appearance.Alt}>
-                <Icon icon={Shape.Profile} />
-            </Button>
+            {#if get(SettingsStore.state).devmode}
+                <Button tooltip={$_("pages.auth.changeUser")} hook="button-change-user" icon on:click={_ => (showAccounts = true)} appearance={Appearance.Alt}>
+                    <Icon icon={Shape.Profile} />
+                </Button>
+            {/if}
             <Button tooltip={$_("pages.auth.relay")} hook="button-configure-relay" icon on:click={_ => (showConfigureRelay = true)} appearance={Appearance.Alt}>
                 <Icon icon={Shape.Relay} />
             </Button>
